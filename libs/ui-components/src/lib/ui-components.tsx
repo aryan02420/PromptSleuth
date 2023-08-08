21d0fe81eb7479364049a96a2bebfe38e2aa3f71
aryan02420 <aryan02420@gmail.com>
2023-08-08 16:06:39 +0530

    🎉 feat(ui-components): initialize component lib

---
+++ libs/ui-components/src/lib/ui-components.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface UiComponentsProps {}
+
+export function UiComponents(props: UiComponentsProps) {
+  return (
+    <div>
+      <h1>Welcome to UiComponents!</h1>
+    </div>
+  );
+}
+
+export default UiComponents;
